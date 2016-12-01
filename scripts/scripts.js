b93e2d1bee507ccbfba8b5090f19e7d5be1ee805
Michael Gee <michaelgee221@gmail.com>
2016-12-01 13:51:50 -0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,17 +3,6 @@ $(document).ready(function(){
    var scroll_start = 0;
    var startchange = $('nav');
    var offset = startchange.offset();
-   $(document).scroll(function() {
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $('nav').css({
-            'background-color': 'rgba(34,34,34,0.9)',
-            'position': 'fixed'
-          });
-       } else {
-          $('nav').css('background-color', 'transparent');
-       }
-   });
 
 // ANCHOR FUNCTION
   $('a[href*="#"]:not([href="#"])').click(function() {
